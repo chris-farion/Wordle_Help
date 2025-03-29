723e2d164f338209dc8d6d696154fa667060419b
Christopher Farion <christopher.farion@gmail.com>
2025-03-29 09:46:32 -0700

    Added ordered letters by duplicates function

--- Wordle_Helper.py
+++ Wordle_Helper.py
@@ -1,8 +1,26 @@
 LENGTH = 5
 GUESSES = 6
 
-NO = 0      #Letter does not exist in word
-YES = 1     #Letter in correct location
-IN = 2      #Letter in wrong location
-ERROR = 3   #Error
+N = 0       #Letter does not exist in word
+Y = 1       #Letter in correct location
+W = 2       #Letter in wrong location
+X = 3       #Error
+
+CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+available_chars = [CHARS]*LENGTH
+    
+def order_letters_by_duplicates(word):
+    print(word)
+    letters = {}
+    for i,letter in enumerate(word):
+        if letter in letters:
+            letters[letter].append(i)
+        else:
+            letters[letter] = [i]
+
+    sorted_dict = dict(sorted(letters.items(), key=lambda item: len(item[1]), reverse=True))
+    print(sorted_dict)
+    return sorted_dict
+
+test = order_letters_by_duplicates("STSST")
 
