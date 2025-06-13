fa00862bb631c6927312df4eabb7825fe055e118
Christopher Farion <christopher.farion@gmail.com>
2025-06-13 10:29:42 -0700

    Improved performance when eliminating options. Command Line implemented. Still needs work on edge cases.

--- Wordle_by_Letter.py
+++ Wordle_by_Letter.py
@@ -3,6 +3,8 @@ Y = 1       #Letter in correct location
 W = 2       #Letter in wrong location
 X = 3       #Error
 
+WORD_LENGTH = 5
+
 def merge_sort_by_position(words,position):
     Sorted_List = is_list_sorted_by_position(words,position)
     if not Sorted_List:
@@ -27,10 +29,16 @@ def merge_lists_by_position(first_half,second_half,position):
     return_array = []
     while(first_half and second_half):
         if (first_half[0][position] == second_half[0][position]):
-            return_array.append(first_half[0])
-            return_array.append(second_half[0])
-            first_half = first_half[1:]
-            second_half = second_half[1:]
+            if (first_half[0][0] > second_half[0][0]):
+                return_array.append(second_half[0])
+                return_array.append(first_half[0])
+                first_half = first_half[1:]
+                second_half = second_half[1:]
+            else:
+                return_array.append(first_half[0])
+                return_array.append(second_half[0])
+                first_half = first_half[1:]
+                second_half = second_half[1:]
         elif (first_half[0][position] < second_half[0][position]):
             return_array.append(first_half[0])
             first_half = first_half[1:]
@@ -61,7 +69,6 @@ def find_range(words,letter,position):
         lo = lo1 = 0
         hi = hi1 = len(words)-1
 
-    
     while (lo != hi-1 or lo1 != hi1-1):
         if return_lower < 0 and lo != hi-1:
             ptr  = (lo + hi) >> 1
@@ -86,6 +93,17 @@ def find_range(words,letter,position):
     return_lower = hi
     return_upper = lo1+1
     return return_lower,return_upper
+
+def order_letters_by_duplicates(word):
+    letters = {}
+    for i,letter in enumerate(word):
+        if letter in letters:
+            letters[letter].append(i)
+        else:
+            letters[letter] = [i]
+
+    sorted_dict = dict(sorted(letters.items(), key=lambda item: len(item[1]), reverse=True))
+    return sorted_dict
 
 def load_5_letter_words():
     file_path = 'Total_Wordle_Word_Bank.txt'
@@ -143,63 +161,185 @@ def merge_lists(first_half,second_half):
     return return_array
 
 #_________________
-#CLOCK
-#YNNNN
 
-def wrong(words,letter,position,indices):
-    print(f"{letter.upper()} - Wrong")
+def wrong(words,letter,position,count):
+    print(f"{letter.upper()} - Wrong @ {position}")
     word_count = len(words)
+    if word_count == 1:
+        return words
     words = merge_sort_by_position(words,position)
     first,last = find_range(words,letter, position)
     if last == word_count:
-        print(words[first])
-        print(words)
         words = words[:first]
-        print(words)
     else:
         words = words[:first] + words[last:]
     temp = []
+    
     for word in words:
-        if letter in word:
+        if word.count(letter) == count:
             temp.append(word)
     words = temp
     print(f"{first}:{last} with {len(words)} words remaining")
-    return words,indices
+    return words
     
 
 def yes(words,letter,position,indices):
-    print(f"{letter.upper()} - Yes")
+    print(f"{letter.upper()} - Yes @ {position}")
     word_count = len(words)
-    indices = indices[:position]+indices[position+1:]
+    if word_count == 1:
+        return words
+    if position not in indices:
+        return words
+    indices.remove(position)
     words = merge_sort_by_position(words,position)
     first,last = find_range(words,letter, position)
     words = words[first:last]
     print(f"{first}:{last} with {len(words)} words remaining")
-    return words,indices
+    return words
 
-def no(words,letter,positions):
-    print(f"{letter.upper()} - No")
+def no(words,letter,positions, count):
+    print(f"{letter.upper()} - No @ {positions}")
+    if count > 0:
+        return words
     for position in positions:
         word_count = len(words)
+        if word_count == 1:
+            return words
         words = merge_sort_by_position(words,position)
         first,last = find_range(words,letter, position)
         if last == word_count:
-            print(words[first])
-            print(words)
             words = words[:first]
-            print(words)
         else:
             words = words[:first] + words[last:]
         print(f"{first}:{last} with {len(words)} words remaining")
     return words
 
+def enter():
+    cmd = input(">> ")
+    components = cmd.split(' ')
+    num_of_components = len(components)
+
+    if num_of_components == 1:
+        if components[0] == "exit":
+            return "exit"
+        else:
+            return
+    if num_of_components == 2:
+        if components[0] != "wrd":
+            return "Command not recognized. Please use 'wrd'."
+        if components[1] == "help":
+            #Create help menu
+            pass
+        elif components[1] == "-reset":
+            return "reset"
+        elif components[1] == "-sts":
+            return "status"
+        else:
+            return
+            
+                        
+    
+    wordle_keyword = components[0]
+    wordle_word = components[1][1:].lower()
+    wordle_result = components[2][1:].upper()
+    if wordle_keyword != "wrd":
+        print("Error. No wrd command")
+        return word, indices
+    if WORD_LENGTH != len(wordle_word) and len(wordle_word) != len(wordle_result):
+        print("Error. Word and result do not match lengths.")
+        return word, indices
+
+    duplicates = order_letters_by_duplicates(wordle_word)
+    print(f"duplicates dict:\n{duplicates}")
+
+    ref = {}
+
+    for ltr in duplicates:
+        ref[ltr] = 0
+        for ind in duplicates[ltr]:
+            if wordle_result[ind] != 'N':
+                ref[ltr] = ref[ltr] + 1
+    print(f"CC:\n{ref}")
+    
+    guess = {}
+    for i,letter in enumerate(wordle_word):
+        guess[i] = {
+            'letter': letter,
+            'result': wordle_result[i],
+            'score': 0,
+            'cc': ref[letter]
+            }
+
+        if guess[i]['result'] == 'Y':
+            guess[i]['score'] = i + 100
+        elif guess[i]['result'] == 'N':
+            guess[i]['score'] = i + 50
+        elif guess[i]['result'] == 'W':
+            guess[i]['score'] = i
+        else:
+            guess[i]['score'] = -1
+
+
+    sorted_guess = dict(sorted(guess.items(), key=lambda item: item[1]['score'],reverse=True))
+
+    schedule = dict(sorted_guess)
+    
+    return schedule
+
+def exe(words,guess,pos):    
+    for key,value in guess.items():
+        print(f"Index: {key}, Letter: {value['letter']}, Score: {value['score']}")
+        if value['result'] == 'Y':
+            words = yes(words,value['letter'],key,pos)
+        elif value['result'] == 'N':
+            words = no(words,value['letter'],pos,value['cc'])
+        elif value['result'] == 'W':
+            words = wrong(words,value['letter'],key,value['cc'])
+        else:
+            pass
+    return words
+    
 #__________________
 
 full_list_of_words = load_5_letter_words()
-words = full_list_of_words
 
-pos = list(range(5))
+w = full_list_of_words
+p = list(range(WORD_LENGTH))
 
-#Guesses here for now#
+cmd_line = ""
 
-words = merge_sort(words)
+while cmd_line != "exit":
+    cmd_line = enter()
+    if cmd_line != "exit":
+        if cmd_line == "reset":
+            w = full_list_of_words
+        elif cmd_line == "status":
+            w = merge_sort(w)
+            print(w)
+        elif cmd_line is None:
+            pass
+        else:
+            w = exe(w,cmd_line,p)
+
+
+
+
+def bug_reports():
+    '''
+    whiny
+    wrd -churn -nynnw
+    wrd -shiny -nyyyy
+    daisy
+    wrd -liken -nwnnn
+    wrd -audit -wnwwn
+    wrd -daily -yyyny
+    stout
+    wrd -share -ynnnn
+    wrd -split -ynnny
+    wrd -scout -ynyyy
+    wrd -snout -ynyyy
+    wrd -smout -ynyyy
+    '''
+    print("Comments contain bugs")
+    
+    
