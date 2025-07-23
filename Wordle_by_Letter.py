b7edd147318acdde56fb228d71a78d8f691f58f5
Christopher Farion <christopher.farion@gmail.com>
2025-07-22 19:56:10 -0700

    Cleaned up user input commands and improved performance of is_list_sorted functions

--- Wordle_by_Letter.py
+++ Wordle_by_Letter.py
@@ -1,9 +1,13 @@
 import random
+import time
+import timeit
 
-N = 0       #Letter does not exist in word
-Y = 1       #Letter in correct location
-W = 2       #Letter in wrong location
-X = 3       #Error
+CMD_EXIT = "exit"
+CMD_RANDOM = ["rand","random"]
+CMD_STATUS = ["sts","status"]
+CMD_PLAY = "play"
+CMD_RESET = "reset"
+KEYWORD = "wrd"
 
 WORD_LENGTH = 5
 
@@ -31,20 +35,39 @@ def merge_sort_by_position(words,position):
         words = merge_lists_by_position(arr0,arr1,position)
     return words
 
-def is_list_sorted_by_position(list_of_words,position):
+def is_list_sorted_by_position(words,position):
+    #print(f"Using this logic to 'jump' 2 words so that the same work is not reloaded")
     is_Sorted = True
-    word_count = len(list_of_words)
-    for word_index in range((word_count)-1):
-        current_letter = list_of_words[word_index][position]
-        compare_letter = list_of_words[word_index+1][position]
-        if current_letter > compare_letter:
+    word_count = len(words)
+    if word_count <= 1:
+        return is_Sorted
+    compare_A_to_B = True
+    switch_scenario = True
+    letter_A = words[0][position]
+    letter_B = words[1][position]
+    for word_index in range(((word_count)-2)):
+        if compare_A_to_B:
+            if letter_A > letter_B:
+                is_Sorted = False
+                break
+            letter_A = words[word_index+2][position]
+            compare_A_to_B ^= switch_scenario
+        else:
+            if letter_B > letter_A:
+                is_Sorted = False
+                break
+            letter_B = words[word_index+2][position]
+            compare_A_to_B ^= switch_scenario
+    if compare_A_to_B:
+        if letter_A > letter_B:
             is_Sorted = False
-            break
+    else:
+        if letter_B > letter_A:
+            is_Sorted = False
     return is_Sorted
 
 def merge_lists_by_position(first_half,second_half,position):
     return_array = []
-
     # While at least one element is in each array...
     while(first_half and second_half):
         if (first_half[0][position] == second_half[0][position]):
@@ -84,12 +107,27 @@ def merge_sort(words):
 def is_list_sorted(words):
     is_Sorted = True
     word_count = len(words)
-    for word_index in range((word_count)-1):
-        current_word = words[word_index]
-        compare_word = words[word_index+1]
-        if current_word > compare_word:
-            is_Sorted = False
-            break
+    if word_count <= 1:
+        return is_Sorted
+    compare_A_to_B = True
+    switch_scenario = True
+    word_A = words[0]
+    word_B = words[1]
+    for word_index in range(((word_count)-1)):
+        if compare_A_to_B:
+            if word_A > word_B:
+                is_Sorted = False
+                break
+            if word_index+2 < word_count:
+                word_A = words[word_index+2]
+                compare_A_to_B ^= switch_scenario
+        else:
+            if word_B > word_A:
+                is_Sorted = False
+                break
+            if word_index+2 < word_count:
+                word_B = words[word_index+2]
+                compare_A_to_B ^= switch_scenario
     return is_Sorted
 
 def merge_lists(first_half,second_half):
@@ -117,40 +155,35 @@ def merge_lists(first_half,second_half):
 
 def find_range(words,letter,position):
     # Assume list already ordered
-    if len(words) == 1:
+    if len(words) <= 1:
         return_lower = 0
         return_upper = 1
         return return_lower,return_upper
-    
-    lo = lo1 = 0
-    hi = hi1 = len(words)-1
-
+    from_lo = to_lo = 0
+    from_hi = to_hi = len(words)-1
     if words[0][position]==letter:
-        hi = 0
-        lo = hi-1
-        
+        from_hi = 0
+        from_lo = from_hi-1
     if words[len(words)-1][position]==letter:
-        hi1 = len(words)
-        lo1 = hi1-1
-        
-    while (lo != hi-1 or lo1 != hi1-1):
-        if lo != hi-1:
-            ptr  = (lo + hi) >> 1
+        to_hi = len(words)
+        to_lo = to_hi-1
+    while (from_lo != from_hi-1 or to_lo != to_hi-1):
+        if from_lo != from_hi-1:
+            ptr  = (from_lo + from_hi) >> 1
             ltr  = words[ptr][position]
             if ltr < letter:
-                lo = ptr
+                from_lo = ptr
             else:
-                hi = ptr
-        if lo1 != hi1-1:
-            ptr1 = (lo1 + hi1) >> 1
+                from_hi = ptr
+        if to_lo != to_hi-1:
+            ptr1 = (to_lo + to_hi) >> 1
             ltr1 = words[ptr1][position]
             if ltr1 > letter:
-                hi1 = ptr1
+                to_hi = ptr1
             else:
-                lo1 = ptr1
-
-    return_lower = hi
-    return_upper = hi1
+                to_lo = ptr1
+    return_lower = from_hi
+    return_upper = to_hi
     return return_lower,return_upper
 
 def order_letters_by_duplicates(word):
@@ -160,7 +193,6 @@ def order_letters_by_duplicates(word):
             letters[letter].append(i)
         else:
             letters[letter] = [i]
-
     sorted_dict = dict(sorted(letters.items(), key=lambda item: len(item[1]), reverse=True))
     return sorted_dict
 
@@ -170,15 +202,12 @@ def wrong(word_bank, schedule, position, remaining_positions, duplicate_dict):
     word_count = len(word_bank)
     word_bank = merge_sort_by_position(word_bank,position)
     temp = []
-
     singular = False
     count = schedule['char_count']
     ct = len(duplicate_dict[letter])
-
-    #TODO: fix below so that the duplicates are handled correctly(-adder -wwnww)
+    #TODO: fix befrom_low so that the duplicates are handled correctly(-adder -wwnww)
     if count != ct:
         singular = True
-
     for word in word_bank:
         if word.count(letter) == count and singular and word[position] != letter:
             temp.append(word)
@@ -187,9 +216,7 @@ def wrong(word_bank, schedule, position, remaining_positions, duplicate_dict):
         else:
             pass
     word_bank = temp
-    
     return word_bank
-    
 
 def yes(word_bank, schedule, position, remaining_positions, duplicate_dict):
     if position not in remaining_positions:
@@ -211,7 +238,6 @@ def no(word_bank, schedule, position, remaining_positions, duplicate_dict):
     if word_count == 1:
         #TODO Somewhat cheating if there are 2-3 options
         return word_bank
-    
     if len(duplicate_dict[letter]) > 1:
         print(f"{letter.upper()} - No @ {position}")
         word_bank = merge_sort_by_position(word_bank,position)
@@ -235,26 +261,25 @@ def enter():
     cmd = input(">> ")
     components = cmd.split(' ')
     num_of_components = len(components)
-
     if num_of_components == 1:
-        if components[0] == "exit":
-            return "exit"
+        if components[0] == CMD_EXIT:
+            return CMD_EXIT
         else:
             return
     if num_of_components == 2:
-        if components[0] != "wrd":
+        if components[0] != KEYWORD:
             return "Command not recognized. Please use 'wrd'."
         if components[1] == "help":
             #Create help menu
             pass
-        elif components[1] == "reset":
-            return "reset"
-        elif components[1] == "sts":
-            return "status"
-        elif components[1] == "rand":
-            return "random"
-        elif components[1] == "play":
-            return "play"
+        elif components[1] == CMD_RESET:
+            return CMD_RESET
+        elif components[1] in CMD_STATUS:
+            return CMD_STATUS
+        elif components[1] in CMD_RANDOM:
+            return CMD_RANDOM
+        elif components[1] == CMD_PLAY:
+            return CMD_PLAY
         else:
             return
     elif num_of_components > 3:
@@ -262,28 +287,22 @@ def enter():
     else:
         return components
 
-def scheduler(components):        
+def scheduler(components):
     wordle_keyword = components[0]
     wordle_word = components[1][1:].lower()
     wordle_result = components[2][1:].lower()
-    if wordle_keyword != "wrd":
+    if wordle_keyword != KEYWORD:
         print("Error. No wrd command")
         return
-    if WORD_LENGTH != len(wordle_word) and len(wordle_word) != len(wordle_result):
-        print("Error. Word and result do not match lengths.")
+    if WORD_LENGTH != len(wordle_word) and (len(wordle_word) != len(wordle_result)):
         return
-
     duplicates = order_letters_by_duplicates(wordle_word)
-
     ref = {}
-
     for ltr in duplicates:
         ref[ltr] = 0
         for ind in duplicates[ltr]:
             if wordle_result[ind] != 'n':
                 ref[ltr] = ref[ltr] + 1
-                
-    
     guess = {}
     for i,letter in enumerate(wordle_word):
         guess[i] = {
@@ -292,7 +311,6 @@ def scheduler(components):
             'score': 0,
             'char_count': ref[letter]
             }
-
         if guess[i]['result'] == 'y':
             guess[i]['score'] = i + 100
         elif guess[i]['result'] == 'n':
@@ -301,15 +319,11 @@ def scheduler(components):
             guess[i]['score'] = i
         else:
             guess[i]['score'] = -1
-
-
     sorted_guess = dict(sorted(guess.items(), key=lambda item: item[1]['score'],reverse=True))
-
     schedule = dict(sorted_guess)
-    
     return schedule, duplicates
 
-def exe(words,guess,pos,duplicate_info):    
+def exe(words,guess,pos,duplicate_info):
     for key,value in guess.items():
         if value['result'] == 'y':
             words = yes(words,guess[key],key,pos,duplicate_info)
@@ -320,37 +334,42 @@ def exe(words,guess,pos,duplicate_info):
         else:
             pass
     return words
-    
-#__________________
 
 full_list_of_words = load_5_letter_words()
-
 w = full_list_of_words
 p = list(range(WORD_LENGTH))
-
 cmd_line = ""
 
-while cmd_line != "exit":
+#func1 = timeit.timeit(lambda: is_list_sorted_by_position(w,0), number=10000)
+
+#print(f"func0: {func0:.5f}")
+#print(f"func1: {func1:.5f}")
+
+while cmd_line != CMD_EXIT:
     cmd_line = enter()
-    if cmd_line != "exit":
-        if cmd_line == "reset":
+    if cmd_line != CMD_EXIT:
+        if cmd_line == CMD_RESET:
             w = full_list_of_words
             p = list(range(WORD_LENGTH))
-        elif cmd_line == "status":
+        elif cmd_line == CMD_STATUS:
             w = merge_sort(w)
             print(w)
         elif cmd_line is None:
             pass
-        elif cmd_line == "random":
-            rando = random.randint(0,len(w))
+        elif cmd_line == CMD_RANDOM:
+            rando = random.randint(0,len(w)-1)
             print(f"Random word: {w[rando]}")
-        elif cmd_line == "play":
+        elif cmd_line == CMD_PLAY:
             w = full_list_of_words
             p = list(range(WORD_LENGTH))
-            rando = random.randint(0,len(w))          
+            rando = random.randint(0,len(w))
         else:
-            schedule,duplicate_info = scheduler(cmd_line)
-            w = exe(w,schedule,p,duplicate_info)
+            try:
+                schedule,duplicate_info = scheduler(cmd_line)
+                w = exe(w,schedule,p,duplicate_info)
+            except TypeError as e:
+                print("Error code ->", e)
+                print("Incorrect input! Please remember to use '-'.")
 
 
 
@@ -369,8 +388,18 @@ def bug_reports():
     wrd -rangs -wnnnn
     wrd -tuyer -wwnnw
     wrd -court -nnyyy
-    
-    
-    https://wordlearchive.com/144
+
+    merge
+    wrd -stale -nnnny
+    wrd -fence -nynny
+    wrd -pewee -nynny
+    wrd -herye -nyyny
+    wrd -eerie -nyyny #eerie should not be here after pewee
+
+    exult
+    wrd -meant -nwnny
+    wrd -eruct -ynyny #No words left after this guess
+
+    https://wordlearchive.com/192
     '''
     print("Comments contain bugs")
