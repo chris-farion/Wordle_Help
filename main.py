304ac9c401e1292dfa54e5e267a2e4a0591d22d7
Christopher Farion <christopher.farion@gmail.com>
2025-12-02 16:04:04 -0800

    Fixing order of functions in main

--- main.py
+++ main.py
@@ -4,30 +4,6 @@ full_list_of_words = load_5_letter_words()
 w = full_list_of_words
 cmd_line = ""
 
-while cmd_line != CMD_EXIT:
-    cmd_line = enter()
-    if cmd_line != CMD_EXIT:
-        if cmd_line == CMD_RESET:
-            w = full_list_of_words
-        elif cmd_line == CMD_STATUS:
-            w = merge_sort(w)
-            print(w)
-        elif cmd_line is None:
-            pass
-        elif cmd_line == CMD_RANDOM:
-            rando = random.randint(0,len(w)-1)
-            print(f"Random word: {w[rando]}")
-        elif cmd_line == CMD_PLAY:
-            w = full_list_of_words
-            rando = random.randint(0,len(w))
-            play(w[rando])
-        else:
-            try:
-                schedule,duplicates = scheduler(cmd_line)
-                w = exe(w,schedule,duplicates)
-            except TypeError as e:
-                print("Error code ->", e)
-
 def play(word):
     word_dict = {}
     for index,letter in enumerate(word):
@@ -83,4 +59,28 @@ def play(word):
             else:
                 response_str += f"{result[current_letter].letter}"
         print(f"{response_str}")
+
+while cmd_line != CMD_EXIT:
+    cmd_line = enter()
+    if cmd_line != CMD_EXIT:
+        if cmd_line == CMD_RESET:
+            w = full_list_of_words
+        elif cmd_line == CMD_STATUS:
+            w = merge_sort(w)
+            print(w)
+        elif cmd_line is None:
+            pass
+        elif cmd_line == CMD_RANDOM:
+            rando = random.randint(0,len(w)-1)
+            print(f"Random word: {w[rando]}")
+        elif cmd_line == CMD_PLAY:
+            w = full_list_of_words
+            rando = random.randint(0,len(w))
+            play(w[rando])
+        else:
+            try:
+                schedule,duplicates = scheduler(cmd_line)
+                w = exe(w,schedule,duplicates)
+            except TypeError as e:
+                print("Error code ->", e)
 
